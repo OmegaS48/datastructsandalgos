053d1bb7bb79c180786b3f1f7613f5e03552038a
Shaad <shaadahmadbh@gmail.com>
2026-03-06 03:30:49 +0530

    Add files via upload

---
+++ printhello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Shark Amen";
+    return 0;
+}
---
+++ printsum.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<3+67<<endl;
+    cout<<3+90;
+    return 0;
+}
